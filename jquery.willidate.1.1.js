131c1fce144633681e2dbfebc4b3e7588acdc0ae
willemodendaal <willem.odendaal@gmail.com>
2011-10-30 16:58:22 +0200

    Now showing required popup.

--- jquery.willidate.1.1.js
+++ jquery.willidate.1.1.js
@@ -1,11 +1,22 @@
 (function( $ ) {
   
-	$.willidate = function(rules) {
+	$.willidate = function(rules, options) {
 		if (!rules)
 		{
 			throw('rules required');
 		}
 		
+		var defaults = {
+		  popupLocation : 'right', // right, bottom
+		  hideOnHover : true
+		};
+		
+		var settings = $.extend({}, defaults, options );
+		
+		//Hide validation messages
+		$('.validation-error').hide();
+		
+		//Get errors...
 		var errorControlsAndMessages = [];
 		$.each(rules, function(index, rule) 
 		{
@@ -17,24 +28,52 @@
 			}
 		});
 		
-		renderErrors(errorControlsAndMessages);
+		//Render errors...
+		renderErrors(errorControlsAndMessages, settings);
+
+		//Additional script
+		if (settings.hideOnHover == true)
+		{
+			$('.validation-error').mouseenter(function() { $(this).hide() } );
+		}
 		
 		return (errorControlsAndMessages.length == 0);
 	};
   
   
-	function renderErrors(errorControlsAndMessages)
+	function renderErrors(errorControlsAndMessages, settings)
 	{
 		$.each(errorControlsAndMessages, function(index, result) 
 		{
-			var ctrl = $(result.control);
-			if (ctrl.length == 0)
+			var $ctrl = $(result.control);
+			if ($ctrl.length == 0)
 			{
 				alert("Unable to validate. Control '" + result.control + "' not found" );
 			}
+
+			var $popupElement = $(getPopupHtml(result.message, result.control));
+			$popupElement.insertAfter($ctrl);
+
+			var pos = $ctrl.position();
+			var width = $ctrl.width();
+			var height = $ctrl.height();
 			
-			ctrl.hide();
+			if (settings.popupLocation == 'right')
+			{
+				$popupElement.css({ "left": (pos.left + width + 5) + "px", "top":pos.top + "px" }).show();
+			}
+			else if (settings.popupLocation == 'bottom')
+			{
+				$popupElement.css({ "left": pos.left + "px", "top": (pos.top + height + 5) + "px" }).show();
+			}
+			
+			$popupElement.show();
 		});
+	}
+	
+	function getPopupHtml(message, controlSelector)
+	{
+		return "<div style=\"position:absolute; display:none;\" for=\"" + controlSelector + "\" class=\"validation-error\">" + message + "</div>";
 	}
   
   
